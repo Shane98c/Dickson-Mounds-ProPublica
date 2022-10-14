de7683330d54d5d4e0984a319067ccafa560b748
Shane98c <shane98c@GMAIL.COM>
2022-10-14 11:59:04 -0700

    raster working

---
+++ mapExtentFeature.js
@@ -0,0 +1,21 @@
+const mapExtentFeature = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      properties: {},
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [-87.25, 42.75],
+            [-87.18, 36.74],
+            [-91.95, 36.67],
+            [-91.95, 42.77],
+            [-87.25, 42.75],
+          ],
+        ],
+      },
+    },
+  ],
+};
